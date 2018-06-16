153487ad35538f5227c50cbee0bbda7bb4cc383a
corytveten <corytveten@gmail.com>
2018-06-16 15:48:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -131,7 +131,7 @@ class Owner
 
   def walk_dogs
     @pets.collect do |species, instances|
-      if species == dogs
+      if species == :dogs
         instances.each do |dog|
           dog.mood = "happy"
         end
